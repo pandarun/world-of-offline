971e9943f46e4831a8d94da6a516dfd3744090dd
Roman Melonikov <oildollar@gmail.com>
2013-12-14 03:00:30 +0400

    just for melnikov commit

---
+++ woo/WooAuth/Utils/StringEncryptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+namespace WooAuth.Utils
+{
+    public static class StringEncryptor
+    {
+        public static string Encrypt(this string plaintextValue)
+        {
+            var plaintextBytes = Encoding.UTF8.GetBytes(plaintextValue);
+            return Convert.ToBase64String(MachineKey.Protect(plaintextBytes, "Authentication token"));
+        }
+
+        public static string Decrypt(this string encryptedValue)
+        {
+            try
+            {
+                var decryptedBytes = MachineKey.Unprotect(Convert.FromBase64String(encryptedValue), "Authentication token");
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+            catch
+            {
+                throw new SecurityException("token forgery");
+            }
+        }
+    }
+}
